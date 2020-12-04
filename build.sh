e41bc11ecfe1f36f77813edc5f13b327b0f17f14
johnmart19 <johnivan19999@gmail.com>
2020-12-04 08:23:54 +0200

    Upstream Buildtools to Proton 12 and LTO

--- build.sh
+++ build.sh
@@ -1,73 +1,36 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
-# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
-# Android Kernel Build Script
+export PATH="$HOME/proton/bin:$PATH"
+SECONDS=0
+ZIPNAME="Redline-Reborn-R-Onclite-$(date '+%Y%m%d-%H%M').zip"
 
-# Main environtment
-KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG=onc-perf_defconfig
-
-# Export
-export ARCH=arm64
-export CROSS_COMPILE
-export CROSS_COMPILE_ARM32
-
-# Main Staff
-
-clang_bin="$HOME/toolchains/proton-clang/bin"
-gcc_prefix64="aarch64-linux-gnu-"
-gcc_prefix32="arm-linux-gnueabi-"
-CROSS_COMPILE="aarch64-linux-gnu-"
-CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
-
-_ksetup_old_path="$PATH"
-export PATH="$clang_bin:$PATH"
-
-# Index of variables for cleanup in unsetup
-_ksetup_vars+=(
-	clang_bin
-	gcc_prefix64
-	gcc_prefix32
-	jobs
-	kmake_flags
-)
-
-kmake_flags+=(
-	CC="clang"
-	AR="llvm-ar"
-	NM="llvm-nm"
-	OBJCOPY="llvm-objcopy"
-	OBJDUMP="llvm-objdump"
-	STRIP="llvm-strip"
-
-	CROSS_COMPILE="$gcc_prefix64"
-	CROSS_COMPILE_ARM32="$gcc_prefix32"
-)
-
-# Build start
-make O=out $CONFIG
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-
-if ! [ -a $KERN_IMG ]; then
-    echo "Build error!"
-    exit 1
+if ! [ -d "$HOME/proton" ]; then
+echo "Proton clang not found! Cloning..."
+if ! git clone -q https://github.com/kdrag0n/proton-clang --depth=1 --single-branch ~/proton; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
 fi
 
-cd $ZIP_DIR
-make clean &>/dev/null
-cd ..
+mkdir -p out
+make O=out ARCH=arm64 onc-perf_defconfig
 
-# For MIUI Build
-# Credit Adek Maulana <adek@techdro.id>
-OUTDIR="$KERNEL_DIR/out/"
+if [[ $1 == "-r" || $1 == "--regen" ]]; then
+cp out/.config arch/arm64/configs/onc-perf_defconfig
+echo -e "\nRegened defconfig succesfully!"
+exit 0
+else
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+fi
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+git clone -q https://github.com/johnmart19/AnyKernel3 -b lto
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+
+OUTDIR="$HOME/redline/out/"
 VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel3/modules/vendor/lib/modules"
 
-STRIP="$HOME/toolchains/proton-clang/aarch64-linux-gnu/bin/strip$(echo "$(find "$HOME/toolchains/proton-clang/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+STRIP="$HOME/proton/aarch64-linux-gnu/bin/strip$(echo "$(find "$HOME/proton/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
             sed -e 's/gcc/strip/')"
 for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
     "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
@@ -77,14 +40,19 @@ for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
             "${MODULES}"
     find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
 done
-cd libufdt/src && python mkdtboimg.py create $OUTDIR/arch/arm64/boot/dtbo.img $OUTDIR/arch/arm64/boot/dts/qcom/*.dtbo
-echo -e "\n(i) Done moving modules"
 
-cd $ZIP_DIR
-cp $KERN_IMG zImage
-cp $OUTDIR/arch/arm64/boot/dtbo.img $ZIP_DIR
-make normal &>/dev/null
-echo "Flashable zip generated under $ZIP_DIR."
+cp out/arch/arm64/boot/dtbo.img AnyKernel3
+cd AnyKernel3
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
-# Build end
-
+rm -rf AnyKernel3
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+if command -v gdrive &> /dev/null; then
+gdrive upload --share $ZIPNAME
+else
+echo "Zip: $ZIPNAME"
+fi
+rm -rf out/arch/arm64/boot
+else
+echo -e "\nCompilation failed!"
+fi
